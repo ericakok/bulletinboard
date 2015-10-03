521e16a0d57fb60a5f2485122c46a2840fdf8d95
woonheeCho <wcho@stetson.edu>
2015-10-04 06:03:37 +0900

    added date and event name to database

--- posts.js
+++ posts.js
@@ -1,12 +1,16 @@
 window.addEventListener("load", function () {
   var form = document.getElementById("postform");
   var loc = document.getElementById("location");
+  var en = document.getElementById("eventName");
+  var dt = document.getElementById("date");
   var FD = new FormData(form);
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     var Post = Parse.Object.extend("Post");
     var post = new Post();
-    post.set("Title", loc.value);
+    post.set("Location", loc.value);
+	post.set("EventName", en.value);
+	post.set("Date", dt.value);
     post.save(null, {
       success: function(post) {
         // Execute any logic that should take place after the object is saved.
